dfbcbf0b8efe6897e0db36d10c135adc2eb8d4e0
zwb <zhuangweibin@stbl.cc>
2019-10-21 13:53:24 +0800

    aa

--- src/main/java/com/zhuang/workflow/autoconfigure/MyWorkflowProperties.java
+++ src/main/java/com/zhuang/workflow/autoconfigure/MyWorkflowProperties.java
@@ -7,23 +7,4 @@ import java.util.List;
 @ConfigurationProperties(prefix = "my.workflow")
 public class MyWorkflowProperties {
 
-    private List<String> dbExecutionHandlers;
-
-    public Boolean getUnderscoreNaming() {
-        return underscoreNaming;
-    }
-
-    public void setUnderscoreNaming(Boolean underscoreNaming) {
-        this.underscoreNaming = underscoreNaming;
-    }
-
-    private Boolean underscoreNaming;
-
-    public List<String> getDbExecutionHandlers() {
-        return dbExecutionHandlers;
-    }
-
-    public void setDbExecutionHandlers(List<String> dbExecutionHandlers) {
-        this.dbExecutionHandlers = dbExecutionHandlers;
-    }
 }
